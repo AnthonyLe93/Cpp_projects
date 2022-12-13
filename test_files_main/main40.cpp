e4424089272e4cb589e7a3ab77eb5246e7c3fecd
Anthony Le <anthony.le@bristol.ac.uk>
2022-12-13 11:36:02 +0000

    static assert example

---
+++ test_files_main/main40.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <type_traits>
+
+/* Type traits */
+
+template<typename T>
+T Divide(T a, T b){
+    static_assert(std::is_floating_point<T>::value, "Only floating point type supported");
+    if(std::is_floating_point<T>::value == false){
+        std::cout << "Use floating point types only" << std::endl;
+        return 0;
+    }
+    return a / b;
+}
+
+template<typename T>
+void Check(T &&){
+    std::cout << std::boolalpha;
+    std::cout 
+    << "Is reference? " 
+    << std::is_reference<T>::value 
+    << std::endl;
+
+    std::cout 
+    << "After removing: " 
+    << std::is_reference<typename std::remove_reference<T>::type>::value
+    << std::endl;
+}
+
+int main40(){
+    // std::cout
+    // << std::boolalpha
+    // << "Is integer? "
+    // << std::is_integral<int>::value 
+    // << std::endl;
+
+    std::cout << Divide(5.0f, 2.0f) << std::endl;
+    // Check(5);
+    // int a {5};
+    // Check(a);
+
+    return 0;
+}
+
+
+
